39b4a57dec1ed9695fa5cba68a8221d8162c0904
Kluck <kluck1812@gmail.com>
2021-11-25 12:23:16 +0300

    def_test_add_new_pet_done

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ class PetFriends:
         )
 
         headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
-        res = requests.get(self.base_url + '/api/pets', headers=headers, data=data)
+        res = requests.post(self.base_url + '/api/pets', headers=headers, data=data)
         status = res.status_code
         result = ''
 
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,18 +1,39 @@
 from app import PetFriends
 from settings import user_email, user_passwd
+import os
 
 
 pf = PetFriends()
 
 
 def test_get_api_key_valid_user(email=user_email, passwd=user_passwd):
+    """ Проверяем что запрос api ключа возвращает статус 200 и в тезультате содержится слово key"""
+
     status, result = pf.get_api_key(email, passwd)
     assert status == 200
-    assert result is not None
+    assert 'key' in result
 
 
 def test_get_list_of_pets_with_valid_key(filter=''):
+    """ Проверяем что запрос всех питомцев возвращает не пустой список.
+    Для этого сначала получаем api ключ и сохраняем в переменную auth_key. Далее используя этого ключ
+    запрашиваем список всех питомцев и проверяем что список не пустой.
+    Доступное значение параметра filter - 'my_pets' либо '' """
+
     _, auth_key = pf.get_api_key(user_email, user_passwd)
     status, result = pf.get_list_of_pets(auth_key, filter)
     assert status == 200
     assert len(result['pets']) > 0
+
+
+def test_add_new_pet_with_valid_key(name='Гаага', animal_type='Гусь',
+                                    age='3', pet_photo='images/goose.jpg'):
+    """Проверяем что можно добавить питомца с корректными данными"""
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(user_email, user_passwd)
+
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+
+    assert status == 200
+    assert result['name'] == name
